46a47a17d95dfa5c721ebf5a9c0f92ae36b09b9c
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2024-12-02 13:05:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,36 @@ const images = [
         src: "https://fxggxt.com/wp-content/uploads/2024/12/HotGuysFuck-The-Explicit-Intimacy-of-Mike-Beaumonte-Lindsay-Lee.jpg",
         name: "[20241202] HotGuysFuck – The Explicit Intimacy of Mike Beaumonte & Lindsay Lee!",
         downloadLink: "https://dood.li/d/j3dn4ii6uhhu"
+    },
+	{
+        src: "https://fxggxt.com/wp-content/uploads/2024/11/HotGuysFuck-Super-Tall-Kolby-Gigante-Dominates-Little-Spinner-Thalia-Rhea.jpg",
+        name: "[20241202] HotGuysFuck – Super Tall Kolby Gigante Dominates Little Spinner Thalia Rhea!",
+        downloadLink: "https://dood.li/d/wzm8mony56cj"
+    },
+	{
+        src: "https://fxggxt.com/wp-content/uploads/2024/11/HotGuysFuck-Endless-Throat-and-a-Rock-Hard-Cock-Chris-Stevens-Fuck-Sarah-Lace.jpg",
+        name: "[20241202] HotGuysFuck – Endless Throat and a Rock Hard Cock! Chris Stevens Fuck Sarah Lace!",
+        downloadLink: "https://dood.li/d/3uf7w349aaqj"
+    },
+	{
+        src: "https://fxggxt.com/wp-content/uploads/2024/11/HotGuysFuck-Best-Friends-Run-Train-On-Slutty-Blonde-BTS.jpg",
+        name: "[20241202] HotGuysFuck – Best Friends Run Train On Slutty Blonde BTS",
+        downloadLink: "https://dood.li/d/ci3lil6km198"
+    },
+	{
+        src: "https://fxggxt.com/wp-content/uploads/2024/11/HotGuysFuck-Un-Cut-18-Year-Old-Latin-Hottie-Jay-Raw-Meets-Industry-Pro-Blonde-Bombshell-in-Scorching-First-Guy-Girl-Scene.jpg",
+        name: "[20241202] HotGuysFuck – Un-Cut 18-Year-Old Latin Hottie Jay Raw Meets Industry Pro Blonde Bombshell in Scorching First Guy-Girl Scene!",
+        downloadLink: "https://dood.li/d/8as3lurey4dg"
+    },
+	{
+        src: "https://fxggxt.com/wp-content/uploads/2024/11/HotGuysFuck-Mike-Iron-Finally-Gets-His-Flowers-with-Sweet-Sophia.jpg",
+        name: "[20241202] HotGuysFuck – Mike Iron Finally Gets His Flowers with Sweet Sophia!",
+        downloadLink: "https://dood.li/d/v0oekhhwznap"
+    },
+	{
+        src: "https://fxggxt.com/wp-content/uploads/2024/11/HotGuysFuck-Live-Cam-Love-Birds-Trey-Beacher-Allison-Lox-Have-Been-Practicing-All-Week-For-This.jpg",
+        name: "[20241202] HotGuysFuck – Live-Cam Love Birds Trey Beacher & Allison Lox Have Been Practicing All Week For This!",
+        downloadLink: "https://dood.li/d/1mofpt6n0bwq"
     }
 ];
 
